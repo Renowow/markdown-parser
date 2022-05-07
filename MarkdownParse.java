f9fac668f7a153f669e6c1b2cc0945584cfcee2e
Lei Chen <cs15lsp22ari@ieng6-201.ucsd.edu>
2022-05-07 13:26:45 -0700

    Let's try new game

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -22,7 +22,7 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
+        //this is a change
         return toReturn;
     }
 
